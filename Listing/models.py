1a827f3a55fae0709abcfd64976d56052ad6bb4a
Marv0912 <marvin.fajardo.s@icloud.com>
2023-06-08 17:46:28 -0400

    migrations for property model

--- Listing/models.py
+++ Listing/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Property(models.Model):
+    name= models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    # price = models.Integ
+    # status = models.
+    # bedrooms = models.
+    # bathrooms = models.
+    # description = models.
+    # location = models.
+    # sqft = models.
+    # date_listed = models.
--- goldenlionproperties/settings.py
+++ goldenlionproperties/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Listing',
+    
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'Listing',
+    'property',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +77,14 @@ WSGI_APPLICATION = "goldenlionproperties.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'glp',      # The name of your database
+        'USER': 'marvinfajardo',   # Your PostgreSQL username
+        'PASSWORD': 'NivRam7878',   # Your PostgreSQL password
+        'HOST': 'localhost',    # The hostname or IP address of your PostgreSQL server
+        'PORT': '5432',         # The port number of your PostgreSQL server
     }
 }
 
