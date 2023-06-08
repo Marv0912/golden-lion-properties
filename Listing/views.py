f3b614ffad0c0dfb3a85491a38e0ce7821e29b4d
Marv0912 <marvin.fajardo.s@icloud.com>
2023-06-08 19:34:01 -0400

    post request for listing form

--- Listing/views.py
+++ Listing/views.py
@@ -10,3 +10,12 @@ def listing(request):
 def contactUs(request):
     return render(request, 'contactUs/contactUs.html')
 
+def create_listing(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else: 
+            form = ListingForm()
+        return render(request, 'create_listing.html', {'form': form})
