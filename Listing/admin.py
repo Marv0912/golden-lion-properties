b134dbfd9e77d8b7eb6e1cbb123a859df87e4a26
Marv0912 <marvin.fajardo.s@icloud.com>
2023-06-09 19:34:34 -0400

    register contact message in admin interface

--- Listing/admin.py
+++ Listing/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Property
+from .models import Property, ContactMessage
 from .forms import ListingForm
 
 class ListingAdmin(admin.ModelAdmin):
     form = ListingForm
 
-admin.site.register(Property, ListingAdmin)
+admin.site.register(Property, ListingAdmin, ContactMessage)
